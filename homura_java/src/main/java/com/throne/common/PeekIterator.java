4bb70a3cf0c62ead1bfa2f127aad8d69b6b8dcea
buzhadui <daimian0715@qq.com>
2020-06-24 22:31:13 +0800

    java version:实现自定义的迭代器用于处理代码文件流，并增加对应单元测试

--- homura_java/src/main/java/com/throne/common/PeekIterator.java
+++ homura_java/src/main/java/com/throne/common/PeekIterator.java
@@ -1,4 +1,90 @@
 package com.throne.common;
 
-public class PeekIteratro {
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+/**
+ * 将代码文件解析成字符流后，通过该类读取，
+ * 本质上是一个迭代器，对字符流进行迭代
+ *
+ * @param <T>
+ */
+public class PeekIterator<T> implements Iterator<T> {
+    private static final int CACHE_SIZE = 10;
+
+    private Iterator<T> iterator;
+
+    // 用于缓存因预览而取出的元素，先进先出
+    private LinkedList<T> outCache = new LinkedList<>();
+
+    // 结束符
+    private T _endToken = null;
+
+    // _endToken已被同步到缓存中的标志
+    private boolean endFlag = false;
+
+    public PeekIterator(Stream<T> stream) {
+        this.iterator = stream.iterator();
+    }
+
+    public PeekIterator(Stream<T> stream, T endToken) {
+        this.iterator = stream.iterator();
+        this._endToken = endToken;
+    }
+
+    /**
+     * todo: 太罗嗦了，有时间重新改写下这段注释
+     * 当缓存为空，流中无法继续迭代出元素，
+     * 同时_endToken也在之前已经被同步缓存中（缓存为空说明_endToken此时已经被消费了），
+     * 则表明无法继续迭代
+     * 存在一种可能的情况，即缓存为空，流中无下一个元素，但_endToken还没有被同步给缓存，
+     * 此时，如果_endToken为null，实际上表明整个PeekIterator无法再释放出元素了，hasNext()结果应为false，
+     * 如果_endToken不为null，则还可以释放一个元素，hasNext()结果应为true，
+     * 在同步缓存时，如果_endToken为null不会被放入缓存，反之则会放入缓存，
+     * 因此_endToken是否为null也需要和endFlag一起纳入判断逻辑中
+     *
+     * @return 可以继续迭代为true，反之false
+     */
+    @Override
+    public boolean hasNext() {
+        return this.outCache.size() != 0 || this.iterator.hasNext() || (!this.endFlag || this._endToken != null);
+    }
+
+    @Override
+    public T next() {
+        syncElementsToCache();
+        return this.outCache.poll();
+    }
+
+    /**
+     * 将流中的元素或_endToken取出并放入缓存中，直到缓存填满或流中没有元素为止
+     * 核心方法，对流和_endToken的操作都先经过缓存同步后，集中成为对缓存的操作，
+     * 从而避免既操作缓存，又操作流和_endToken时因逻辑不统一出现错漏或超限的问题。
+     */
+    private void syncElementsToCache() {
+        while ((this.outCache.size() < CACHE_SIZE) && !this.endFlag) {
+            if (this.iterator.hasNext()) {
+                this.outCache.add(this.iterator.next());
+            } else {
+                if (this._endToken != null) {
+                    this.outCache.add(this._endToken);
+                }
+                this.endFlag = true;
+            }
+        }
+    }
+
+    /**
+     * 预览流中的下一个元素，首先判断缓存中是否有元素，
+     * 没有再从流中拿，流中也没有，返回null
+     * 保持每次只看缓存的第一个元素，
+     *
+     * @return 流或缓存中的第一个元素
+     */
+    public T peek() {
+        syncElementsToCache();
+        return this.outCache.peekFirst();
+    }
+
 }
---
+++ homura_java/src/test/java/com/throne/lexer/PeekIteratorTests.java
@@ -0,0 +1,34 @@
+package com.throne.lexer;
+
+import com.throne.common.PeekIterator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * com.throne.lexer
+ * Created by throne on 2020/6/24
+ */
+public class PeekIteratorTests {
+
+    @Test
+    public void testPeek(){
+        String source = "abcdefg";
+        PeekIterator<Character> peekIterator = new PeekIterator<>(source.chars().mapToObj(c -> (char) c));
+
+        Assertions.assertEquals('a', peekIterator.next());
+        Assertions.assertEquals('b', peekIterator.next());
+        Assertions.assertEquals('c', peekIterator.next());
+        Assertions.assertEquals('d', peekIterator.peek());
+        Assertions.assertEquals(Boolean.TRUE, peekIterator.hasNext());
+        Assertions.assertEquals('d', peekIterator.peek());
+        Assertions.assertEquals('d', peekIterator.next());
+        Assertions.assertEquals('e', peekIterator.next());
+        Assertions.assertEquals('f', peekIterator.peek());
+        Assertions.assertEquals('f', peekIterator.next());
+        Assertions.assertEquals('g', peekIterator.peek());
+        Assertions.assertEquals('g', peekIterator.next());
+        Assertions.assertEquals(Boolean.FALSE, peekIterator.hasNext());
+        Assertions.assertNull(peekIterator.peek());
+
+    }
+}
